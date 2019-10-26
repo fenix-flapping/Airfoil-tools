eede0e9ccb44fef11a7d9b8e54159ae6bb28eda5
fenix-flapping <55887460+fenix-flapping@users.noreply.github.com>
2019-10-25 21:23:24 -0300

    Update NACA-4D.py

--- NACA-4D.py
+++ NACA-4D.py
@@ -4,138 +4,46 @@ import math
 import matplotlib.pyplot as plt #se puede borrar usado para verificar
 
 
-def NACA_4D( name, chord=1, n_points=100 ,separation=False):
+def NACA_4D( name, chord=1, n_points=100 ,separation=False ):
     coef=[]
     # NACA MPXX, classification of the 4 digits
     if len(name) == 4:
         for x in name:
             coef.append(x)
-        XX = int(coef[2]+coef[3])/100
         M = int(coef[0])/100
         P = int(coef[1])/10
+        XX = int(coef[2]+coef[3])/100
     else:
         print("The NACA number must be four digits")
     
-    
+    # Coeficientes de la función distribucion de espesor, el valor a4 define si el TE es abierto o cerrado
     a0 = 0.2969 ; a1 = -0.1260 ; a2 = -0.3516 ; a3 = 0.2843;
     if separation == False:
         a4 = -0.1036
     else:
         a4 = -0.1015
-        
+    '''
+    Generacion de puntos "x" con una distribucion 1-cos(tita)
+    yt funcion distribucion de espesor
+    yc funcion de la linea de curvatura media, dyc su derivada
+    tita pendiente en radianes de la linea de curvatura media
+    '''
     tita = np.linspace(math.pi/2, 0, n_points, True)
     xc = [1-math.cos(tita[x]) for x in range(len(tita))]
     yt = [(5*XX)*((a0*math.sqrt(xc[x]))+a1*(xc[x])+a2*(math.pow(xc[x],2))+a3*(math.pow(xc[x],3))+a4*(math.pow(xc[x],4))) for x in range(len(tita))]
     yc = [(M/(math.pow(1-P,2)))*(1-2*P+2*P*xc[x]-(math.pow(xc[x],2))) for x in range(len(xc)) if xc[x]>=P]+[(M/(math.pow(P,2)))*(2*P*xc[x]-(math.pow(xc[x],2))) for x in range(len(xc)) if xc[x]<P]
     dyc = [(2*M/(math.pow(1-P,2)))*(P-xc[x]) for x in range(len(xc)) if xc[x]>=P]+[((2*M)/math.pow(P,2))*(P-xc[x]) for x in range(len(xc)) if xc[x]<P]
     t = [(math.atan(dyc[x])) for x in range(len(dyc))]
-
+    
+    # X,Y coordenas del extrados(u) e intrados(l) del Perfil
     Xu = [round((xc[x]-yt[x]*math.sin(t[x]))*chord,4) for x in range(len(xc))]
     Xl = [round((xc[x]+yt[x]*math.sin(t[x]))*chord,4) for x in range(len(xc))]
     Yu = [round(yc[x]+yt[x]*math.cos(t[x])*chord,4) for x in range(len(xc))]
     Yl = [round((yc[x]-yt[x]*math.cos(t[x]))*chord,4) for x in range(len(xc))]
 
+    # Union de las curvas extrados e intrados, salida coords con listado de puntos
     X = Xu + Xl[::-1]
     Y = Yu + Yl[::-1]
     coords = [( X[x],Y[x] ) for x in range( len(X) )]
 
     return coords
-
-coords=NACA_4D("4409")
-
-
-fig = plt.figure()
-ax = plt.axes()
-plt.plot(X,Y, 'o', color='black')
-plt.show()
-
-#a0=0.2969;a1=-0.1260;a2=-0.3516;a3=0.2843;
-#% Leading edge cerrado, para abierto se usa a4=0.1015;
-#a4=-0.1036;
-#
-#h=0.01;  %first layer
-#% generacion de puntos
-#n=250;   % numero de puntos
-#
-#% max thickness
-#XX=18/100;  %max thickness
-#M=3/100;    %primer digito
-#P=3/10;     %segundo digito
-#c=0.32;     %cuerda
-#
-#tita=0:(pi/2/(n-1)):pi/2;
-#xc=cos(tita);   % Distribucion cosenoidal de puntos "x"
-#%xc=0:1/n:1
-#
-#for i=1:length(xc)
-#    if xc(i)<P
-#        yc(i)=(M/(P^2))*(2*P*xc(i)-(xc(i)^2));
-#        dyc(i)=((2*M)/P^2)*(P-xc(i));
-#    else
-#        yc(i)=(M/(1-P)^2)*(1-2*P+2*P*xc(i)-(xc(i).^2));
-#        dyc(i)=(2*M/(1-P)^2)*(P-xc(i));
-#    end
-#    yt(i)=(5*XX)*(a0*sqrt(xc(i))+a1*(xc(i))+a2*(xc(i)^2)+a3*(xc(i)^3)+a4*(xc(i)^4));
-#    t(i)=atan(dyc(i));
-#    
-#    Xu(i)=(xc(i)-yt(i)*sin(t(i)))*c;
-#    Xl(i)=(xc(i)+yt(i)*sin(t(i)))*c;
-#    
-#    Yu(i)=(yc(i)+yt(i)*cos(t(i)))*c;
-#    Yl(i)=(yc(i)-yt(i)*cos(t(i)))*c;
-#end
-#
-#% Filtrado y reacomodo de datos
-#% Falta agregar && c*.25<X(i)>c*.90
-#Xa=([Xu flip(Xl)]);
-#Ya=([Yu flip(Yl)]);
-#s=0;
-#Y(1)=0;
-#X(1)=c;
-#for i=2:length(Xa)-1
-#    if abs(Ya(i)-Ya(i+1))>1e-4  %Filtrado de diferencias pequeñas
-#        if abs(Ya(i))>1e-4  % Filtrado de valores cercanos al cero de Y
-#            Y(i-s)=Ya(i);
-#            X(i-s)=Xa(i);
-#        else
-#            Y(i-s)=0;
-#        end
-#        if abs(Xa(i))>1e-4 %Filtrado de valores cercanos al cero de X
-#            X(i-s)=Xa(i);
-#        else
-#            X(i-s)=0;
-#        end
-#    else
-#        s=s+1;
-#    end
-#end
-#Y(length(Y)+1)=0;
-#X(length(X)+1)=c;
-#
-#% for i=1:length(xc)-1
-#%     tt(i)=atan((Yu(i+1)-Yu(i))/((Xu(i+1)-Xu(i))));
-#%     if i<=1
-#%         Xblu(i)=c+h;
-#%         Yblu(i)=0;
-#%     else
-#%         tt(i)=(Yu(i+1)-Yu(i))/((Xu(i+1)-Xu(i)));
-#%         Xblu(i)=Xu(i)-h*sin(t(i));
-#%         Yblu(i)=Yu(i)+h*cos(t(i));  % Buscar la derivada real y no la de la curvatura cambiar t(i)
-#%     end
-#% end
-#
-#% Escritura de Datos
-#% DATA=[transpose(X),transpose(Y),zeros(length(X),1)];  %3D
-#DATA=[transpose(X),transpose(Y)];   %2D
-#dlmwrite('NACA-Puntos.txt',DATA,'delimiter','\t','precision','%1.4f')
-#
-#% Ploteo de perfil y linea de curvatura
-#plot(Xu,Yu)
-#hold on
-#plot(Xl,Yl)
-#axis equal
-#plot(Xu,yc*c)
-#grid on
-#hold off
-#
-
