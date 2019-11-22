f1adf27f8940cabd6c191b74754e4746f1dc196b
fenix-flapping <pablo_a983@hotmail.com>
2019-11-22 13:21:48 -0300

    Create found_division_index.py

---
+++ found_division_index.py
@@ -0,0 +1,30 @@
+'''
+Funcion que busca valores especificos de la ordenada del extrados e intrados
+y devuelve el indice en que se ubica del vector de coordenadas del perfil.
+No posee proteccion ante pedido de valores erroneos o fuera de escala
+'''
+def found_division_index(coords , division):
+    point_division_extr= [0] # El primer punto es siempre el "0"
+    point_division_intr = []
+    for div in division:
+        for i in range(len(coords)):
+                j=0
+                k=0
+                if coords[i][0]<div and j==0:
+                    divextr=i
+                    j=1
+                if coords[-i][0]<div and k==0:
+                    divintr=len(coords)-i
+                    k=1
+                if j==1 and k==1:
+                    break
+        point_division_extr.append(divextr)
+        point_division_intr.append(divintr)
+    # El primer punto del intrados es el valor minimo del vector de coordenadas del perfil
+    point_division_intr.append(coords.index(min(coords))) 
+    point_division=point_division_extr[:]+point_division_intr[::-1]
+    return point_division
+
+
+
+ 
